c63a989c6453728cb79427d19d033ca9078521dc
Shuxin-Lai <shuxinlai001@gmail.com>
2023-02-28 23:23:03 +0800

    chore: update port

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8004,
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
